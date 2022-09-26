361f62e5d78a9ba93cd7ce7d94e5bad069ab75f7
Vlad Kapustsin <kapustsin.u@titan-t.com>
2022-09-26 22:04:51 +0300

    SCSS + tailwind init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
+import { useState } from 'react';
 import './App.scss';
+import styles from './App.scss'
+import {initialPlaces} from './place.data';
 
 function App() {
+  /* const [places, setPlaces] = useState(initialPlaces); */
+
   return (
     <div className="App">
-      <h1>SCSS basics</h1>
-      <button>Go to channel</button>
+      <h1>SCSS Basics</h1>
+      <button>go to channel</button>
+      <button className='active'>go to channel</button>
+      {/* <div className='asd'>
+        <h2>SCSS</h2>
+        <button>asfasdf</button>
+      </div> */}
+      {/* { places.map(place => (
+          <div key={place.name}>
+            <img src={place.image} alt={place.name} />
+            <div className={styles.heading}>{place.name}</div>
+            <div className={styles.country}>{place.country}</div>
+          </div>))} */}
     </div>
   );
 }
